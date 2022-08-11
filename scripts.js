09e29e80bae34397ecb2971f0596c0edc5661593
Alexey Balayan <alexey.balayan@icloud.com>
2022-08-11 15:04:29 +0300

    update

--- scripts.js
+++ scripts.js
@@ -96,3 +96,19 @@ document.addEventListener("keydown", function (e) {
         popupClose(popupActive);
     }
 })
+
+let header__burger = document.querySelector('.header_burger');
+let header_menu = document.querySelector('.header_menu');
+let back = document.querySelector('body');
+let header__list = document.querySelector('.header_list');
+
+header__burger.onclick = function(){
+    header__burger.classList.toggle('active');
+    header_menu.classList.toggle('active');
+    back.classList.toggle('lock');
+}
+
+header__list.onclick = function () {
+    header__list.classList.remove('active');
+    back.classList.toggle('lock');
+}
